017916c003ab942fe37688436d9ba49f1757df4b
Taras Shevchenko <tshevchenko@rightandabove.com>
2023-02-10 23:22:52 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import spinner from './assets/icons/Spinner.gif'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={spinner} alt="spinner" />
+      <div className="text">Coming soon...</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
