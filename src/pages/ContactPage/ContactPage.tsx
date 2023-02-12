8f087cee565730212881524e9dddb310d5f78f0f
Taras Shevchenko <tshevchenko@rightandabove.com>
2023-02-12 02:35:40 +0200

    updated 0.3

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
 export const ContactPage = () => {
-  return <div className="contact-container">contact</div>
+  return (
+    <div className="contact-container">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur culpa deleniti distinctio earum,
+      eligendi eum eveniet ex, explicabo facere id, iure laborum laudantium libero minus nam natus neque nisi nobis
+      nulla quasi quibusdam quidem quo ratione sapiente similique soluta suscipit tempora tempore ullam veritatis vero
+      voluptates voluptatibus. A aliquam dolorem eos, esse et expedita inventore ipsam laudantium natus omnis optio
+      perferendis placeat quod saepe sed sint tempore temporibus ullam veniam voluptatibus? Aliquid asperiores autem
+      culpa, eaque exercitationem expedita incidunt iusto necessitatibus neque numquam perferendis, porro possimus
+      quaerat quam, sapiente temporibus voluptatibus? Accusantium ad inventore, molestiae nemo officiis possimus quam
+      reprehenderit repudiandae sit temporibus? Aliquam at doloribus, labore natus nemo nulla similique sint tempore
+      veritatis? Adipisci autem culpa deleniti doloremque ipsam maxime mollitia odio perferendis praesentium
+      reprehenderit! Eveniet excepturi laboriosam modi molestiae natus ut voluptatum. Accusantium aliquid aut commodi
+      consectetur, consequatur deserunt dolorum ducimus eaque eius eos esse incidunt, inventore libero maxime minima
+      modi nemo nisi nulla officiis perspiciatis porro possimus quia quis, quos reprehenderit suscipit vel voluptatum.
+      Accusantium architecto asperiores aut beatae commodi consectetur consequatur cumque doloribus ducimus et illum in
+      ipsam ipsum iste itaque labore laborum, non odio omnis quas, quidem rem repellat, repellendus temporibus ut vel
+      voluptatum.
+    </div>
+  )
 }
